31c9f51f59d5eed5d7830f51119e4feef0f361f1
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2015-01-20 12:11:03 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+for file in _*; do
+    if [[ -e $HOME/${file/_/.} ]]; then
+        echo "$HOME/${file/_/.} already exists" >&2
+    else
+        ln -s $PWD/$file $HOME/${file/_/.}
+    fi
+done
+
+git submodule update
