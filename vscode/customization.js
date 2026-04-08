baed26e20d189924ae64e70a9d8e5e6503e84db7
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2026-04-08 11:54:29 -0400

    Clean up custom css and js

--- vscode/customization.js
+++ vscode/customization.js
@@ -1,54 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     const checkElement = setInterval(() => {
         const commandDialog = document.querySelector(".quick-input-widget");
-        if (commandDialog) {
-          // Apply the blur effect immediately if the command dialog is visible
-          if (commandDialog.style.display !== "none") {
-            runMyScript();
-          }
-            // Create an DOM observer to 'listen' for changes in element's attribute.
-            const observer = new MutationObserver((mutations) => {
-                mutations.forEach((mutation) => {
-                    if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-                        if (commandDialog.style.display === 'none') {
-                            handleEscape();
-                        } else {
-                            // If the .quick-input-widget element (command palette) is in the DOM
-                            // but no inline style display: none, show the backdrop blur.
-                            runMyScript();
-                        }
-                    }
-                });
-            });
-
-            observer.observe(commandDialog, { attributes: true });
-
-            // Clear the interval once the observer is set
-            clearInterval(checkElement);
-        } else {
-            console.log("Command dialog not found yet. Retrying...");
+        if (!commandDialog) {
+            console.debug("Command dialog not found.  Retrying ... ");
+            return;
         }
+
+        shouldShowBlur = commandDialog.style.display !== "none";
+
+        // Observer only gets attached after 1st run; make that not look ugly
+        if (shouldShowBlur) { addBlurContainer() }
+
+        // Create an DOM observer to 'listen' for changes in element's attribute.
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                    const shouldShowBlur = commandDialog.style.display !== "none";
+                    if (shouldShowBlur) { addBlurContainer() }
+                    else                { detachBlurContainer() }
+                }
+            });
+        });
+
+        observer.observe(commandDialog, { attributes: true });
+
+        // Clear the interval once the observer is set
+        clearInterval(checkElement);
     }, 500); // Check every 500ms
 
-    // Execute when command palette was launched.
-    document.addEventListener('keydown', function(event) {
-        if ((event.metaKey || event.ctrlKey) && event.key === 'p') {
-            event.preventDefault();
-            runMyScript();
-        } else if (event.key === 'Escape' || event.key === 'Esc') {
-            event.preventDefault();
-            handleEscape();
-        }
-    });
-
-    // Ensure the escape key event listener is at the document level
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'Escape' || event.key === 'Esc') {
-            handleEscape();
-        }
-    }, true);
-
-    function runMyScript() {
+    function addBlurContainer() {
         const targetDiv = document.querySelector(".monaco-workbench");
 
         // Remove existing element if it already exists
@@ -70,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Remove the backdrop blur from the DOM when esc key is pressed.
-    function handleEscape() {
+    function detachBlurContainer() {
         const element = document.getElementById("command-blur");
         if (element) {
             element.click();
