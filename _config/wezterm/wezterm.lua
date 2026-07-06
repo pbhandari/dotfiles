e2ad5577cb29a514e335f29bb7a9d5e39495738a
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2026-07-06 13:56:17 -0400

    Bump wezterm font size to 14 and prioritize Nerd Font fallback

--- _config/wezterm/wezterm.lua
+++ _config/wezterm/wezterm.lua
@@ -9,10 +9,10 @@ config.color_scheme = 'Molokai'
 -- Set the Font to Monaco
 config.font = wezterm.font_with_fallback {
   'Monaco',
-  'Hiragino Sans',
   'Symbols Nerd Font Mono',
+  'Hiragino Sans',
 }
-config.font_size = 12.0 -- Adjust this size to your preference
+config.font_size = 14.0 -- Adjust this size to your preference
 
 config.hide_tab_bar_if_only_one_tab = true
 config.default_prog = {"zsh", "-l"}
