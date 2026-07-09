45178db8de195a7e85543014ae9aa3fa23ad7c8a
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2026-07-09 11:51:11 -0400

    nvim: rework lualine sections (searchcount, diff, diagnostics, encoding)

--- _config/nvim/lua/plugins/ui.lua
+++ _config/nvim/lua/plugins/ui.lua
@@ -1,38 +1,57 @@
--- Statusline + indent guides (replacing vim-airline and indentLine).
 return {
-  -- lualine replaces vim-airline: pure Lua, much faster startup.
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    event = 'VeryLazy',
-    opts = {
-      options = {
-        theme = 'molokai',
-        icons_enabled = true,
-        section_separators = '',
-        component_separators = '',
-        globalstatus = true,
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
-      },
-    },
-  },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        event = 'VeryLazy',
+        opts = {
+            options = {
+                theme = 'molokai',
+                icons_enabled = true,
+                section_separators = '',
+                component_separators = '',
+                globalstatus = true,
+            },
+            sections = {
+                lualine_a = { 'mode' },
 
-  -- indent-blankline replaces yggdroot/indentLine (treesitter-aware, no
-  -- conceallevel hacks).
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    event = { 'BufReadPost', 'BufNewFile' },
-    opts = {
-      indent = { char = '│' },
-      scope = { enabled = false },
+                lualine_b = { 'searchcount' },
+                lualine_c = {
+
+                    {
+                        'filename',
+                        path = 1,
+                        file_status = true,
+                    },
+                },
+                lualine_x = {
+                    {
+                        'filetype',
+                        colored = false,
+                        icon_only = true
+                    },
+                },
+                lualine_y = {
+                    {
+                        'diff',
+                        colored = true,
+                        fmt = function(str) return str ~= '' and '±' or '' end,
+                    },
+                    { 'location', }
+                },
+                lualine_z = {
+                    { 'diagnostic' },
+                    { 'encoding',  show_bomb = true },
+                },
+            },
+        },
     },
-  },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        event = { 'BufReadPost', 'BufNewFile' },
+        opts = {
+            indent = { char = '│' },
+            scope = { enabled = false },
+        },
+    },
 }
